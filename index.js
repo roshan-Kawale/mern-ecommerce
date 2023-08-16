baa6b4735544ad888a114d031aa043488f8d4ba8
Roshan <roshankawale43@gmail.com>
2023-08-16 15:56:02 +0530

    Cart, User, Orders and Auth added

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const productRouter = require('./routes/Products')
 const brandRouter = require('./routes/Brands')
 const categoryRouter = require('./routes/Categories')
-// const cors = require('cors')
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Carts');
+const ordersRouter = require('./routes/Orders');
 
-// server.use(cors({
-//     exposedHeaders : ['X-Total_Count']
-// }))
+
+//middlewares
+
+server.use(cors({
+    exposedHeaders : ['X-Total_Count']
+}))
 server.use(express.json())
 server.use('/products', productRouter.router)
 server.use('/brands', brandRouter.router)
 server.use('/categories', categoryRouter.router)
+server.use('/users', usersRouter.router)
+server.use('/auth', authRouter.router)
+server.use('/cart', cartRouter.router)
+server.use('/orders', ordersRouter.router)
 
 main().catch(err=>console.error(err))
 
